689393b2e1b85c8d9a7ab150bc69d8e81eae08d2
Miclenzy <technolenz@gmail.com>
2024-09-01 09:28:04 -0700

    Added Permissions activity and addded new colors

--- app/src/main/java/com/technolenz/securegallery/SplashActivity.kt
+++ app/src/main/java/com/technolenz/securegallery/SplashActivity.kt
@@ -1,4 +1,20 @@
 package com.technolenz.securegallery
 
-class SplashActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, PermissionsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
 }
